003ea8f9ae6a478c9f9d2e778e0e019bccf0f37a
Yash Kushwaha <yashkc2025@gmail.com>
2023-09-23 11:19:30 +0530

    added rithgika

--- src/app/team/core.js
+++ src/app/team/core.js
@@ -1,124 +1,146 @@
 export const coreMembers = [
-    {
-        "name": "Yash Kumar यश कुमार",
-        "email": "22f3000472@ds.study.iitm.ac.in",
-        "links" : {
-            "github": "https://github.com/yashkc2025",
-            "linkedin": "https://www.linkedin.com/in/yashkc2025/",
-            "portfolio": "https://me.yashkc.com",
-        },
-        "photo" : "pics/yash.jpg",
-        "tagLine": "Writing Poetries in the mountains 🗻",
-        "shortIntro" : "You'll often catch me cruising through the winding mountain roads, as if the twists and turns were custom-made for my two-wheeled companion 'The Ronin' 🏍️🏔️",
-        "position" : "Technical Head"
+  {
+    name: "Yash Kumar यश कुमार",
+    email: "22f3000472@ds.study.iitm.ac.in",
+    links: {
+      github: "https://github.com/yashkc2025",
+      linkedin: "https://www.linkedin.com/in/yashkc2025/",
+      portfolio: "https://me.yashkc.com",
     },
-    {
-        name: "Neelotpala",
-        email: "",
-        links: {
-            linkedin: "https://www.linkedin.com/in/neelotpala-india-3159a3220/"
-        },
-        photo: "pics/neelotpala.webp",
-        shortIntro: "Neelotpala, the Advisor in Chief of the 'Deva Bhasha' Sanskrit society at IITM. She adores hosting and theater, captivating audiences with her charm. Her love for Sanskrit drives her to preserve its beauty and promote its significance.        ",
-        position: "Advisor in Chief"
+    photo: "pics/yash.jpg",
+    tagLine: "Writing Poetries in the mountains 🗻",
+    shortIntro:
+      "You'll often catch me cruising through the winding mountain roads, as if the twists and turns were custom-made for my two-wheeled companion 'The Ronin' 🏍️🏔️",
+    position: "Technical Head",
+  },
+  {
+    name: "Neelotpala",
+    email: "",
+    links: {
+      linkedin: "https://www.linkedin.com/in/neelotpala-india-3159a3220/",
     },
-    {
-        name: "Niyati Rapria",
-        email: "",
-        tagLine: "The 'chill pill' leader",
-        links: {
-            linkedin: "https://www.linkedin.com/in/niyati-rapria-a7339126a",
-            instagram: "https://www.instagram.com/_niyati.rapria",
-            twitter: "https://twitter.com/@_niyati.rapria"
-        },
-        photo: "pics/niyati.webp",
-        shortIntro: "With a passion for Sanskrit, she's the 'chill pill' leader. Her infectious enthusiasm and creativity bring the language to life. From dancing to painting, she's a well-rounded soul",
-        position: "Documentation Head"
+    photo: "pics/neelotpala.webp",
+    shortIntro:
+      "Neelotpala, the Advisor in Chief of the 'Deva Bhasha' Sanskrit society at IITM. She adores hosting and theater, captivating audiences with her charm. Her love for Sanskrit drives her to preserve its beauty and promote its significance.        ",
+    position: "Advisor in Chief",
+  },
+  {
+    name: "Niyati Rapria",
+    email: "",
+    tagLine: "The 'chill pill' leader",
+    links: {
+      linkedin: "https://www.linkedin.com/in/niyati-rapria-a7339126a",
+      instagram: "https://www.instagram.com/_niyati.rapria",
+      twitter: "https://twitter.com/@_niyati.rapria",
     },
-    {
-        name: "Ayush S Khade",
-        email: "",
-        links: {},
-        photo: "pics/ayush.webp",
-        shortIntro: "With a burning passion for Sanskrit and culture, he's all about making connections that enrich lives. Bridging the gap between modern society and ancient wisdom.",
-        position: "Deputy PR Head"
+    photo: "pics/niyati.webp",
+    shortIntro:
+      "With a passion for Sanskrit, she's the 'chill pill' leader. Her infectious enthusiasm and creativity bring the language to life. From dancing to painting, she's a well-rounded soul",
+    position: "Documentation Head",
+  },
+  {
+    name: "Ayush S Khade",
+    email: "",
+    links: {},
+    photo: "pics/ayush.webp",
+    shortIntro:
+      "With a burning passion for Sanskrit and culture, he's all about making connections that enrich lives. Bridging the gap between modern society and ancient wisdom.",
+    position: "Deputy PR Head",
+  },
+  {
+    name: "Krishnan Lakshmi Narayana",
+    email: "22f3002565@ds.study.iitm.ac.in",
+    links: {
+      github: "https://github.com/agentfortuner",
+      linkedin: "https://www.linkedin.com/in/kln12",
+      instagram: "https://www.instagram.com/agentfortuner_cc1/",
     },
-    {
-        "name": "Krishnan Lakshmi Narayana",
-        "email": "22f3002565@ds.study.iitm.ac.in",
-        "links" : {
-            "github": "https://github.com/agentfortuner",
-            "linkedin": "https://www.linkedin.com/in/kln12",
-            "instagram": "https://www.instagram.com/agentfortuner_cc1/",
-        },
-        tagLine: "The guy with connections",
-        "photo" : "pics/krishnan.webp",
-        "shortIntro" : "The ambitious PR head of Deva Bhasha at IIT Madras. Passionate about Sanskrit, he rocks social media to connect globally and promote the language's rich heritage. ",
-        "position" : "Public Relations Head"
+    tagLine: "The guy with connections",
+    photo: "pics/krishnan.webp",
+    shortIntro:
+      "The ambitious PR head of Deva Bhasha at IIT Madras. Passionate about Sanskrit, he rocks social media to connect globally and promote the language's rich heritage. ",
+    position: "Public Relations Head",
+  },
+  {
+    name: "Nidhi Dande",
+    email: "",
+    links: {
+      instagram:
+        "https://instagram.com/nidhi_dande?utm_source=qr&igshid=ZDc4ODBmNjlmNQ%3D%3D",
     },
-    {
-        name: "Nidhi Dande",
-        email: "",
-        links: {
-            instagram: "https://instagram.com/nidhi_dande?utm_source=qr&igshid=ZDc4ODBmNjlmNQ%3D%3D"
-        },
-        tagLine: "The girl who misses her metro !",
-        photo: "pics/nidhi.webp",
-        shortIntro: "Passionate about Sanskrit and India's heritage, she's a pro at connecting with people. A keen observer of human behavior, she's all about appreciating unique qualities and diving into books for self-discovery",
-        position: "Deputy PR Head"
+    tagLine: "The girl who misses her metro !",
+    photo: "pics/nidhi.webp",
+    shortIntro:
+      "Passionate about Sanskrit and India's heritage, she's a pro at connecting with people. A keen observer of human behavior, she's all about appreciating unique qualities and diving into books for self-discovery",
+    position: "Deputy PR Head",
+  },
+  {
+    name: "Ojasvi Kumar",
+    email: "",
+    links: {
+      linkedin: "https://www.linkedin.com/in/ojasvi-kumar-125a65141",
+      instagram:
+        "https://instagram.com/ojasvi170?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
     },
-    {
-        name: "Ojasvi Kumar",
-        email: "",
-        links: {
-            linkedin: "https://www.linkedin.com/in/ojasvi-kumar-125a65141",
-            instagram: "https://instagram.com/ojasvi170?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
-        },
-        tagLine: "Love for Dinosaurs 🦖",
-        photo: "pics/ojasvi.webp",
-        shortIntro: "With a curious mind and a passion for dinosaurs, he's a unique gem! Asking thought-provoking questions, he explores Sanskrit texts and connects ancient life with modern management.",
-        position: "Management CO-Head"
+    tagLine: "Love for Dinosaurs 🦖",
+    photo: "pics/ojasvi.webp",
+    shortIntro:
+      "With a curious mind and a passion for dinosaurs, he's a unique gem! Asking thought-provoking questions, he explores Sanskrit texts and connects ancient life with modern management.",
+    position: "Management CO-Head",
+  },
+  {
+    name: "Sneha Kulkarni",
+    email: "",
+    links: {},
+    photo: "pics/sneha.webp",
+    shortIntro:
+      "A 10-year tech whiz! Balancing like a boss—she rocks software and home life! In Deva Bhasha Core, her leadership, communication, and approachability make her the go-to leader !",
+    position: "Management CO-Head",
+  },
+  {
+    name: "Prathmesh Atkare",
+    email: "22f3000787@ds.study.iitm.ac.in",
+    tagLine: "The design guy",
+    links: {
+      instagram:
+        "http://www.instagram.com/atkareprathmesh14](http://www.instagram.com/atkareprathmesh14",
+      linkedin: "https://www.linkedin.com/in/prathmesh-atkare-4436aa269/",
+      twitter: "https://twitter.com/prathmesh1401",
     },
-    {
-        name: "Sneha Kulkarni",
-        email: "",
-        links: {},
-        photo: "pics/sneha.webp",
-        shortIntro: "A 10-year tech whiz! Balancing like a boss—she rocks software and home life! In Deva Bhasha Core, her leadership, communication, and approachability make her the go-to leader !",
-        position: "Management CO-Head"
+    photo: "pics/prathmesh.webp",
+    shortIntro:
+      "Although I'm not particularly interested in design, I find myself currently stuck in the design department. My true passion lies in the tech field.",
+    position: "Design Head",
+  },
+  {
+    name: "Shivam Raj",
+    email: "",
+    tagLine: "The cool tech head",
+    links: {},
+    photo: "pics/shivam.webp",
+    shortIntro:
+      "the tech prodigy and co-head of Technical at IITM BS Sanskrit Society! 🌟 His coding passion and automation wizardry have transformed the society, making Sanskrit more accessible and impactful",
+    position: "Co-Technical Head",
+  },
+  {
+    name: "Anagha P Bharadwaj",
+    email: "",
+    tagLine: "Cultures with Words of Wisdom ✨",
+    links: {
+      linkedin: "https://www.linkedin.com/in/anagha-p-bharadwaj-aaba7725a/",
     },
-    {
-        name: "Prathmesh Atkare",
-        email: "22f3000787@ds.study.iitm.ac.in",
-        tagLine: "The design guy",
-        links: {
-            instagram: "http://www.instagram.com/atkareprathmesh14](http://www.instagram.com/atkareprathmesh14",
-            linkedin: "https://www.linkedin.com/in/prathmesh-atkare-4436aa269/",
-            twitter: "https://twitter.com/prathmesh1401",
-        },
-        photo: "pics/prathmesh.webp",
-        shortIntro: "Although I'm not particularly interested in design, I find myself currently stuck in the design department. My true passion lies in the tech field.",
-        position: "Design Head"
-    },
-    {
-        name: "Shivam Raj",
-        email: "",
-        tagLine: "The cool tech head",
-        links: {},
-        photo: "pics/shivam.webp",
-        shortIntro: "the tech prodigy and co-head of Technical at IITM BS Sanskrit Society! 🌟 His coding passion and automation wizardry have transformed the society, making Sanskrit more accessible and impactful",
-        position: "Co-Technical Head"
-    },
-    {
-        name: "Anagha P Bharadwaj",
-        email: "",
-        tagLine: "Cultures with Words of Wisdom ✨",
-        links: {
-            linkedin: "https://www.linkedin.com/in/anagha-p-bharadwaj-aaba7725a/",
-        },
-        photo: "pics/anagha.webp",
-        shortIntro: "Passionate about Sanskrit and linguistic diversity, she rocks interactive teaching and cultural events, promoting unity on campus. Her poetic finesse weaves traditional charm with modern vibes. ",
-        position: "Pedagogy Head"
-    }
-
-]
+    photo: "pics/anagha.webp",
+    shortIntro:
+      "Passionate about Sanskrit and linguistic diversity, she rocks interactive teaching and cultural events, promoting unity on campus. Her poetic finesse weaves traditional charm with modern vibes. ",
+    position: "Pedagogy Head",
+  },
+  {
+    name: "Rithika",
+    email: "",
+    tagLine: "",
+    links: {},
+    photo: "pics/rithika.jpg",
+    shortIntro: "I bring a wealth of experience and creativity to the table, particularly in the realm of design and team management.",
+    position: "Design and Documentation Head",
+  },
+];
